8f247aaaab07c0c5af9f11b8aed9c147e55365b5
Dom Smith <domsmith0416@gmail.com>
2022-11-20 00:06:16 -0500

    Update cube.c

--- cube.c
+++ cube.c
@@ -21,22 +21,26 @@ float ooz;
 int xp, yp;
 int idx;
 
-float calculateX(int i, int j, int k) {
+float calculateX(int i, int j, int k) 
+{
   return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) +
          j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C);
 }
 
-float calculateY(int i, int j, int k) {
+float calculateY(int i, int j, int k) 
+{
   return j * cos(A) * cos(C) + k * sin(A) * cos(C) -
          j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(C) -
          i * cos(B) * sin(C);
 }
 
-float calculateZ(int i, int j, int k) {
+float calculateZ(int i, int j, int k) 
+{
   return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
 }
 
-void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch) {
+void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch) 
+{
   x = calculateX(cubeX, cubeY, cubeZ);
   y = calculateY(cubeX, cubeY, cubeZ);
   z = calculateZ(cubeX, cubeY, cubeZ) + distanceFromCam;
@@ -55,7 +59,8 @@ void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch) {
   }
 }
 
-int main() {
+int main() 
+{
   printf("\x1b[2J");
   while (1) {
     memset(buffer, backgroundASCIICode, width * height);
